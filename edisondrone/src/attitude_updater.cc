583946a917d2c3f50c16c2bba98a3a3d4a86d7d1
Gregory Haynes <greg@greghaynes.net>
2016-03-10 06:35:29 +0000

    Don't use magenetometor for attitude, accelero should be negative.

--- edisondrone/src/attitude_updater.cc
+++ edisondrone/src/attitude_updater.cc
@@ -9,8 +9,9 @@ AttitudeUpdater::AttitudeUpdater(unsigned int imu_update_ms)
     : m_9dof_updater(imu_update_ms,
                      std::bind(&AttitudeUpdater::update9Dof, this))
     , m_9dof(0x6B, 0x1D) 
+    , m_estimator()
     , m_gyro_res(4.2760585 / 32768.0)
-    , m_accel_res(2.0 / 32768.0)
+    , m_accel_res(-2.0 / 32768.0)
     , m_mag_res(2.0 / 32768.0) {
 }
 
@@ -38,10 +39,5 @@ void AttitudeUpdater::update9Dof() {
                        m_9dof.ax * m_accel_res,
                        m_9dof.ay * m_accel_res,
                        m_9dof.az * m_accel_res,
-                       m_9dof.mx * m_mag_res,
-                       m_9dof.my * m_mag_res,
-                       m_9dof.mz * m_mag_res);
-    std::cout << "roll: " << m_estimator.eulerRoll()
-              << " pitch: " << m_estimator.eulerPitch()
-              << " yaw: " << m_estimator.eulerYaw() << std::endl;
+                       0, 0, 0);
 }
