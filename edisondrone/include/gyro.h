e86451a1f6ca044611d0f3f1d1b0bc34fcc6e717
Gregory Haynes <greg@greghaynes.net>
2016-10-15 17:41:20 -0700

    Add sensor and gyro class definitions

---
+++ edisondrone/include/gyro.h
@@ -0,0 +1,17 @@
+#ifndef GYRO_H
+#define GYRO_H
+
+#include "sensor.h"
+#include "sensor-listener.h"
+
+namespace EdisonDrone {
+    class GyroEvent : public SensorEvent {
+    };
+
+    class Gyro : public Sensor {
+        public:
+            void getSenseEvent(GyroEvent *ev) {};
+    };
+}
+
+#endif
---
+++ edisondrone/include/sensor.h
@@ -0,0 +1,12 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+namespace EdisonDrone {
+    class SensorEvent {
+    };
+
+    class Sensor {
+    };
+}
+
+#endif
